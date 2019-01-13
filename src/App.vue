26260c351d32ec262b590c2213b4c23dae33a8ed
kenan814 <741113998.com>
2019-01-13 10:45:15 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
